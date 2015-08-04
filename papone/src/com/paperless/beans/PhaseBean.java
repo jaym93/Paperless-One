ecf198afe3dbc9e6fd958e2b40d1e2db90d6d4c5
pavanpkp33 <pavanpkp33@gmail.com>
2015-08-04 16:45:56 +0530

    Created beans

---
+++ papone/src/com/paperless/beans/PhaseBean.java
@@ -0,0 +1,22 @@
+package com.paperless.beans;
+
+public class PhaseBean {
+	
+	private String phase;
+	private String phaseCount;
+	public String getPhase() {
+		return phase;
+	}
+	public void setPhase(String phase) {
+		this.phase = phase;
+	}
+	public String getPhaseCount() {
+		return phaseCount;
+	}
+	public void setPhaseCount(String phaseCount) {
+		this.phaseCount = phaseCount;
+	}
+	
+	
+
+}
